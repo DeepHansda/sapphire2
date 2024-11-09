c0acc7feae862a3998d444fd1da22a4ea778a57c
DeepHansda <deephansda921@gmail.com>
2024-11-09 22:33:29 +0530

    responsiveness added in frontend

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -2,7 +2,7 @@ function previewImage(event) {
   const fileInput = event.target;
   const preview = document.getElementsByClassName("preview-img");
   const previewContainer = document.getElementById("image-preview");
-console.log(preview)
+  console.log(preview);
   // Display the selected image in the preview container
   const file = fileInput.files[0];
   if (file) {
@@ -23,4 +23,14 @@ console.log(preview)
 function showSliderVal(event) {
   const rangeVal = document.getElementById("rangeVal");
   rangeVal.innerText = event.target.value;
+}
+
+function sidebarHandler() {
+  const sideBar = document.getElementById("sidebar");
+  const isOpen = sideBar.style.left === "0px"; // Check if the sidebar is open
+  if (isOpen) {
+    sideBar.style.left = "-100%"; // Close the sidebar
+  } else {
+    sideBar.style.left = "0px"; // Open the sidebar
+  }
 }
